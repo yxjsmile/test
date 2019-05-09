6d7ab9f337f5588ea82a1cd96fba26b39a49cf01
yxjsmile <957819344@qq.com>
2019-05-09 10:48:49 +0800

    添加了一个语句

--- gg/gg/ViewController.swift
+++ gg/gg/ViewController.swift
@@ -14,8 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("xinjianfenzhi")
+        
+        print("xin")
     }
-
 
 }
 
