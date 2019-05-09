5c19e101bc559c354933b6dfadca516a73a12631
yxjsmile <957819344@qq.com>
2019-05-09 10:02:15 +0800

    初始化测试

---
+++ gg/gg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gg
+//
+//  Created by iMac on 2019/5/9.
+//  Copyright © 2019 iMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
